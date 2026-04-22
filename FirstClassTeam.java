1d684c7a49a25e0f49f314f4a9dd606529319cc6
rayrayrayismyname <rayrayrayismyname@gmail.com>
2026-04-22 15:51:31 +0800

    新增組員「吳昕睿」

--- FirstClassTeam.java
+++ FirstClassTeam.java
@@ -3,5 +3,6 @@ public class FirstClassTeam {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357008\"\n姓名：\"潘鑫哲\"");
         System.out.println("\"學號：\"01357017\"\n姓名：\"黃鼎祐\"");
+        System.out.println("\"學號：\"01357042\"\n姓名：\"吳昕睿\"");
     }
 }
